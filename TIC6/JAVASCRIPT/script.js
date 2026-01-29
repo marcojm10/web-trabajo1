48b58ae967f170b307cfe0f59c1d3c6eeb01d32f
marcojm10 <marco.jmtic@gmail.com>
2026-01-29 09:41:09 -0600

    Add files via upload

--- TIC6/JAVASCRIPT/script.js
+++ TIC6/JAVASCRIPT/script.js
@@ -5,3 +5,10 @@ const boton1=document.getElementById('tilin');
 boton1.addEventListener('click',function(){
     alert("HOLAA");
 });
+function toggleMobileMenu() {
+    const menu = document.getElementById("mobileMenu");
+    if (menu.style.width === "100%") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "100%";
+    }
